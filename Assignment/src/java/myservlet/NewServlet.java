f5f3870a786781413400521215da98b0ff87b32e
caolinh2002 <linhcghe161735@fpt.edu.vn>
2022-10-14 11:08:53 +0700

    Revert "ScheduleStudent"
    This reverts commit a5d7daa15362edf7a504d9cc87ba88fbcf4bf804.

--- Assignment/src/java/myservlet/NewServlet.java
+++ Assignment/src/java/myservlet/NewServlet.java
@@ -2,20 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+package myservlet;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Cao Gia Linh
  */
-@WebServlet(urlPatterns = {"/NewServlet"})
+@WebServlet(name = "NewServlet", urlPatterns = {"/NewServlet"})
 public class NewServlet extends HttpServlet {
 
     /**
